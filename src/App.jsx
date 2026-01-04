fe36440a1f8815e001b85302ef9f00147756d4fa
sushant98k <sushant98k@gmail.com>
2026-01-04 23:02:37 +0530

    Navigation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from './sections/Navbar'
 
 const App = () => {
   return (
-    <div className='container mx-auto'>App</div>
+    <div className='container max-w-7xl'>
+
+      {/* Navbar */}
+      <Navbar />
+      
+      {/* Hero Section */}
+      {/* About Section */}
+      {/* Projects Section */}
+      {/* Experiance Section */}
+      {/* Testimonial Section */}
+      {/* Contact Section */}
+      {/* Footer */}
+
+    </div>
   )
 }
 
